4e6d5e4818e18b78be42c3d9f8077e4b57411e62
Paulo Ricardo <dev.paulorc@gmail.com>
2024-06-30 10:45:46 -0300

    ✏️ CREATE - Tela inicial do projeto.

---
+++ MarkBank-SYSTEM/app/Views/inicial.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mark Bank</title>
+    <link rel="stylesheet" href="<?= base_url('assets/style/inicial.css') ?>">
+</head>
+
+<body>
+    <div class="esquerda">
+        <img class="simbolo" src="<?= base_url('assets/img/logo.png') ?>" alt="Logo vertical">
+    </div>
+    <div class="direita">
+        <div class="div_img">
+            <div class="escuro"></div>
+            <img class="img" src="<?= base_url('assets/img/cidade.jpg') ?>" alt="Cidade">
+        </div>
+        <div class="interior">
+            <h1 class="titulo">Mark Bank</h1>
+            <p class="crie">Crie sua conta agora mesmo!</p>
+            <a href=""><button class="btn">Entrar</button></a>
+            <a href=""><button class="btn">Cadastrar</button></a>
+        </div>
+    </div>
+</body>
+
+</html>
